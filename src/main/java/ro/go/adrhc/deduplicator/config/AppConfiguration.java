f46af029ab10dabb7143df84f69a77d8e971948a
Adrian Petre <adrian.petre@endava.com>
2023-10-20 02:28:14 +0300

    NonInteractiveShellRunnerCustomizer

--- src/main/java/ro/go/adrhc/deduplicator/config/AppConfiguration.java
+++ src/main/java/ro/go/adrhc/deduplicator/config/AppConfiguration.java
@@ -1,17 +1,28 @@
 package ro.go.adrhc.deduplicator.config;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.boot.ApplicationArguments;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.shell.boot.NonInteractiveShellRunnerCustomizer;
 import ro.go.adrhc.util.io.FileSystemUtils;
 
+import java.util.Collections;
+import java.util.List;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 @Configuration
 @RequiredArgsConstructor
 public class AppConfiguration {
 	private final AppProperties appProperties;
+
+	@Bean
+	public NonInteractiveShellRunnerCustomizer nonInteractiveShellRunnerCustomizer() {
+		return shellRunner -> shellRunner.setCommandsFromInputArgs(this::skipSpringBootParams);
+	}
 
 	@Bean
 	public FileSystemUtils fileSystemUtils() {
@@ -21,5 +32,12 @@ public class AppConfiguration {
 	@Bean
 	public ExecutorService metadataExecutorService() {
 		return Executors.newFixedThreadPool(appProperties.getMetadataLoadingThreads());
+	}
+
+	private List<String> skipSpringBootParams(ApplicationArguments appArgs) {
+		String command = Stream.of(appArgs.getSourceArgs())
+				.filter(arg -> !arg.startsWith("--spring."))
+				.collect(Collectors.joining(" "));
+		return command.isBlank() ? List.of() : Collections.singletonList(command);
 	}
 }
