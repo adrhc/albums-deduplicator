688b8c39ac01f9c1940df32ffafd04d4a37b220c
Adrian Petre <adrian.petre@endava.com>
2023-10-20 03:13:09 +0300

    DefaultActualData.java

--- src/main/java/ro/go/adrhc/deduplicator/datasource/index/changes/DefaultActualData.java
+++ src/main/java/ro/go/adrhc/deduplicator/datasource/index/changes/DefaultActualData.java
@@ -6,32 +6,26 @@ import lombok.extern.slf4j.Slf4j;
 
 import java.io.IOException;
 import java.nio.file.Path;
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
-import java.util.Optional;
 import java.util.function.Function;
 
 @RequiredArgsConstructor
 @Slf4j
 public class DefaultActualData<T> implements ActualData<T> {
-	private final Function<String, Optional<T>> documentIdToDataConverter;
+	private final Function<String, T> documentIdToDataConverter;
 	private final List<T> data;
 
 	public static DefaultActualData<Path> actualPaths(
 			SneakySupplier<List<Path>, IOException> dataSupplier) throws IOException {
-		return new DefaultActualData<>(id -> Optional.of(Path.of(id)), dataSupplier.get());
+		return new DefaultActualData<>(Path::of, new ArrayList<>(dataSupplier.get()));
 	}
 
 	@Override
 	public boolean remove(String documentId) {
-		Optional<T> optionalT = documentIdToDataConverter.apply(documentId);
-		if (optionalT.isEmpty()) {
-			log.error("The indexed object identified by {} can't " +
-					"be converted to actual-data type!", documentId);
-			return false;
-		} else {
-			return data.remove(optionalT.get());
-		}
+		T t = documentIdToDataConverter.apply(documentId);
+		return data.remove(t);
 	}
 
 	@Override
