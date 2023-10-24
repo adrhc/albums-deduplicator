b78e76e295a67f8986bf8e47819043df524af055
Adrian <adrhc@outlook.com>
2023-10-24 23:58:47 +0300

    Analyzer analyzer() throws IOException

--- src/main/java/ro/go/adrhc/deduplicator/config/index/LuceneFactoriesConfig.java
+++ src/main/java/ro/go/adrhc/deduplicator/config/index/LuceneFactoriesConfig.java
@@ -7,13 +7,15 @@ import org.springframework.context.annotation.Configuration;
 import ro.go.adrhc.deduplicator.datasource.index.core.LuceneFactories;
 import ro.go.adrhc.persistence.lucene.index.core.analysis.AnalyzerFactory;
 
+import java.io.IOException;
+
 @Configuration
 @RequiredArgsConstructor
 public class LuceneFactoriesConfig {
 	private final LuceneFactories luceneFactories;
 
 	@Bean
-	public Analyzer analyzer() {
+	public Analyzer analyzer() throws IOException {
 		return analyzerFactory().create();
 	}
 
