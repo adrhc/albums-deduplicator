bc94341c710801f2a7a5d50eb8cab5211297ebc3
Adrian <adrhc@outlook.com>
2023-11-20 18:44:34 +0200

    IndexCommands.java

--- src/main/java/ro/go/adrhc/deduplicator/shell/IndexCommands.java
+++ src/main/java/ro/go/adrhc/deduplicator/shell/IndexCommands.java
@@ -1,12 +1,14 @@
 package ro.go.adrhc.deduplicator.shell;
 
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.time.StopWatch;
 import org.springframework.context.ApplicationContext;
 import org.springframework.shell.standard.ShellComponent;
 import org.springframework.shell.standard.ShellMethod;
 import ro.go.adrhc.deduplicator.config.apppaths.ObservableAppPaths;
 import ro.go.adrhc.deduplicator.datasource.metadata.FileMetadata;
 import ro.go.adrhc.persistence.lucene.typedindex.restore.IndexDataSource;
+import ro.go.adrhc.util.StopWatchUtils;
 
 import java.io.IOException;
 import java.nio.file.Path;
@@ -22,8 +24,11 @@ public class IndexCommands extends AbstractCommand {
 	@ShellMethod(key = {"create", "reset"},
 			value = "Create the index at the provided path (remove it first, if exists).")
 	public void reset() throws IOException {
+		StopWatch watch = StopWatchUtils.start();
 		fileMetadataRepository().reset(indexDataSource.loadAll());
-		log.debug("\n{} index created!", appPaths.getIndexPath());
+		watch.stop();
+		log.debug("\n{} index created!\nIndexed {} songs in {}.",
+				appPaths.getIndexPath(), fileMetadataRepository().count(), watch.formatTime());
 	}
 
 	@ShellMethod("Get the index size.")
